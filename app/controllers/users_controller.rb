cd8f43bb17ab8671e80708a4ea6bd17802345247
Gev Gharadaghi <gowithgev@gmail.com>
2015-11-03 15:18:50 -0800

    update user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,25 +5,39 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-def show
-end
+  def show
+        @user = User.find(params[:id])
+
+  end
 
   def new
     @user = User.new
-end
+  end
 
-def edit
-end
+  def edit
+    @user = User.find(params[:id])
 
-def create
+  end
+
+  def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "You have successfully signed up!"
+      flash[:notice] = "You have successfully signed up for Trippin Out! Organize Your Trip Now!"
       redirect_to trip_index_path
     else
       render 'new'
     end
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Your Info Has Been Updated!"
+      redirect_to trip_index_path
+    else
+      render 'edit'
+    end
+end
 
   private
     # Implement Strong Params
